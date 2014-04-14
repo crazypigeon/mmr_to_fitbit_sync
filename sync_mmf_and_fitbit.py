db69f23303a971dc2a08617cbafd106d961ad4d3
Erik Granlund <egranlund@Magnesium.local>
2014-04-13 21:59:58 -0700

    Basic sync functionality working including timezone translation. Still needs duplication checks however

--- sync_mmf_and_fitbit.py
+++ sync_mmf_and_fitbit.py
@@ -1,7 +1,11 @@
 from mapmyfitness import MapMyFitness
 import fitbit
+from datetime import datetime
+from pytz import timezone
 
 from config import *
+
+METERS_IN_A_MILE = 1609.34
 
 mmf = MapMyFitness(api_key=MMF_CLIENT_KEY, access_token=MMF_ACCESS_TOKEN)
 
@@ -11,16 +15,32 @@ fitbit_client = fitbit.Fitbit( FITBIT_CLIENT_KEY, FITBIT_CLIENT_SECRET, user_key
 workouts = mmf.workout.search(user=MMF_USER_ID,activity_type=MMF_ACTIVITY_TYPE)
 
 for workout in workouts:
+  # 2014-04-10 16:24:12+00:00
+
+  meh = workout.start_datetime.strftime( '%Y-%m-%d %H:%M:%S%z' )
+
+  print meh
+
   print workout.name
   print "\t" + str( workout.start_datetime )
   print "\t" + str( workout.distance_total )
   print "\t" + str( workout.elapsed_time_total )
 
-# TODO:
-# 1. Implement startTime conversion
-# 2. Implement date conversion
-# 3. Test to make sure duplication doesn't happen
+  pacific_timezone = timezone('America/Los_Angeles')
+
+  start_time = workout.start_datetime.astimezone( pacific_timezone ).strftime( '%H:%M' )
+  start_date = workout.start_datetime.astimezone( pacific_timezone ).strftime( '%Y-%m-%d' )
 
   # Log the activity in FitBit
-  #fitbit_client.log_activity( {'activityId':FITBIT_ACTIVITY_ID, 'startTime':"12:00", 'durationMillis': 1000 * 60 * 60, 'date' : "2014-04-13", 'distance':"1.23"} )
+  fitbit_client.log_activity(
+     { 
+        'activityId' : FITBIT_ACTIVITY_ID, 
+        'startTime' : start_time,
+        'durationMillis': int( 1000 * workout.elapsed_time_total ),
+        'date' : start_date,
+        'distance' : workout.distance_total / METERS_IN_A_MILE
+     }
+  )
+  
+
   
